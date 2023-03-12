9c9f39db70d26d48214e6230c9938ce12d649117
blacksmithalex <blacksmithalex@mail.ru>
2023-03-12 17:46:57 +0300

    12mar23

---
+++ 19 - 21/Задача 1/21.py
@@ -0,0 +1,18 @@
+def f(x, p):
+    if x >= 30 and (p == 5 or p == 3):
+        return True
+    elif x < 30 and p == 5:
+        return False
+    elif x >= 30:
+        return False
+    else:
+        if p % 2 == 0:
+            return f(x + 2, p + 1) or f(x + 3, p + 1) or f(x * 2, p + 1)
+        else:
+            return f(x + 2, p + 1) and f(x + 3, p + 1) and f(x * 2, p + 1)
+
+for S in range(1, 29 + 1):
+    if f(S, 1):
+        print(S)
+
+#Ответ: 8 9
---
+++ 19 - 21/Задача 4/20.py
@@ -0,0 +1,17 @@
+def f(x, p):
+    if x >= 34 and p == 4:
+        return True
+    elif x < 34 and p == 4:
+        return False
+    elif x >= 34:
+        return False
+    else:
+        if p % 2 == 1:
+            return f(x + 1, p + 1) or f(x + 2, p + 1) or f(x + 3, p + 1) or f(x * 2, p + 1)
+        else:
+            return f(x + 1, p + 1) and f(x + 2, p + 1) and f(x + 3, p + 1) and f(x * 2, p + 1)
+
+for S in range(1, 33 + 1):
+    if f(S, 1):
+        print(S)
+#Ответ: 8 15
